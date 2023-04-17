957168fb2f3285a3dce19e4d4a808f85271a8f7c
dmitry <dmitry_cec2@mail.ru>
2023-04-17 16:40:44 +0300

    commit2

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ IMAGE="alrosa3/jmeter:${JMETER_VERSION}"
 JMETER_HOME=/opt/apache-jmeter-${JMETER_VERSION}
 JMETER_BIN=${JMETER_HOME}/bin
 # Finally run
-docker run --rm --name ${NAME}$1 -i -v ${PWD}:${JMETER_BIN}/project -w ${JMETER_BIN}/project ${IMAGE} $@
+docker run --rm --name ${NAME}$2 -i -v ${PWD}:${JMETER_BIN}/project -w ${JMETER_BIN}/project ${IMAGE} $@
